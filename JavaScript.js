e3418fcf8b8846ace28eef67a16e499871599d97
Hue-OwO <71944049+Hue-OwO@users.noreply.github.com>
2021-02-28 22:30:03 +0000

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -2,50 +2,33 @@ function about() {
   var ab = document.getElementById("about");
   var co = document.getElementById("contact");
   var ph = document.getElementById("photography");
-  var dis = document.getElementById("server");
   if (ab.style.display === "none") {
     ab.style.display = "block";
     co.style.display = "none";
     ph.style.display = "none";
-    dis.style.display = "none";
   }
   }
   function social() {
   var ab = document.getElementById("about");
   var co = document.getElementById("contact");
   var ph = document.getElementById("photography");
-  var dis = document.getElementById("server");
   if (co.style.display === "none") {
     ab.style.display = "none";
     co.style.display = "block";
     ph.style.display = "none";
-    dis.style.display = "none";
   }
   }
     function photography() {
   var ab = document.getElementById("about");
   var co = document.getElementById("contact");
   var ph = document.getElementById("photography");
-  var dis = document.getElementById("server");
   if (ph.style.display === "none") {
     ab.style.display = "none";
     co.style.display = "none";
     ph.style.display = "block";
-    dis.style.display = "none";
   }
   }
-function server() {
-  var ab = document.getElementById("about");
-  var co = document.getElementById("contact");
-  var ph = document.getElementById("photography");
-  var dis = document.getElementById("server");
-  if (ph.style.display === "none") {
-    ab.style.display = "none";
-    co.style.display = "none";
-    ph.style.display = "none";
-    dis.style.display = "block";
-  }
-  }
+
 function rick() {
 window.location = "https://youtu.be/dQw4w9WgXcQ";
 }
