bf58464353ff533706e8b088e2c6afc7a30c0286
Hue-OwO <71944049+Hue-OwO@users.noreply.github.com>
2021-02-28 15:53:47 +0000

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,19 +1,30 @@
-var modal = document.getElementById("images");
-
-var btn = document.getElementById("photosButton");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function about() {
+  var ab = document.getElementById("about");
+  var co = document.getElementById("contact");
+  var ph = document.getElementById("photography");
+  if (ab.style.display === "none") {
+    ab.style.display = "block";
+    co.style.display = "none";
+    ph.style.display = "none";
   }
-}
+  }
+  function social() {
+  var ab = document.getElementById("about");
+  var co = document.getElementById("contact");
+  var ph = document.getElementById("photography");
+  if (co.style.display === "none") {
+    ab.style.display = "none";
+    co.style.display = "block";
+    ph.style.display = "none";
+  }
+  }
+    function photography() {
+  var ab = document.getElementById("about");
+  var co = document.getElementById("contact");
+  var ph = document.getElementById("photography");
+  if (ph.style.display === "none") {
+    ab.style.display = "none";
+    co.style.display = "none";
+    ph.style.display = "block";
+  }
+  }
